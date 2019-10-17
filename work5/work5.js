15678ea5691c2beb69a2f06f7eeb260f4c413541
chenxuhui-cell <401634351@qq.com>
2019-10-17 14:45:58 +0800

    jswork

--- work5/work5.js
+++ work5/work5.js
@@ -1,3 +1,17 @@
+//9X9空表
+str = '<table border="1">'
+for(let a=1;a<10;++a){
+    str += '<tr>'
+    for(let b=1;b<10;++b){
+        str +='<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table1').innerHTML = str
+
+
+
 //倒九九乘法空表
 str = '<table border="1">'
 for(let i=9;i>0;--i){
@@ -9,3 +23,15 @@ for(let i=9;i>0;--i){
 }
 str += '</table>'
 document.getElementById('table2').innerHTML = str
+
+//倒九九乘法表
+str = '<table border="1">'
+for(let c=9;c>0;--c){
+    str += '<tr>'
+    for(var d=c;d>0;--d){
+        str += "<td>" + d + "*" +c + "=" + "</td>";
+    }
+    str +='</tr>'    
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str
